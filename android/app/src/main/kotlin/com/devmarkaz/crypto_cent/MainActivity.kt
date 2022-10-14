1dbbaba6e0071920767f914356cd49f347417605
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2022-10-14 21:30:13 +0500

    init

---
+++ android/app/src/main/kotlin/com/devmarkaz/crypto_cent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devmarkaz.crypto_cent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crypto Cent',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(body: Center(child: Text('Init'))),
+    );
+  }
+}
