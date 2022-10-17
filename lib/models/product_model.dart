322d3148a2d9991da948e956931cb0828248e3c9
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-17 19:06:27 +0500

    image_picker done

---
+++ lib/models/product_model.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ProductModel {
+  final String pid;
+  final double amount;
+  final String colors;
+  final String quantity;
+  final String description;
+  final int timestamp;
+  final String category;
+  final String subCategory;
+  final String createdByUID;
+  final String imageurl;
+  ProductModel({
+    required this.pid,
+    required this.amount,
+    required this.colors,
+    required this.quantity,
+    required this.description,
+    required this.timestamp,
+    required this.category,
+    required this.subCategory,
+    required this.createdByUID,
+    required this.imageurl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'pid': pid,
+      'amount': amount,
+      'colors': colors,
+      'quantity': quantity,
+      'description': description,
+      'timestamp': timestamp,
+      'category': category,
+      'sub_category': subCategory,
+      'created_by_uid': createdByUID,
+      'image_url': imageurl
+    };
+  }
+
+  factory ProductModel.fromMap(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return ProductModel(
+        pid: doc.data()?['pid'] ?? '',
+        amount: doc.data()?['amount'] ?? 0,
+        colors: doc.data()?['colors'] ?? '',
+        quantity: doc.data()?['quantity'] ?? '',
+        description: doc.data()?['description'] ?? '',
+        timestamp: doc.data()?['timestamp'] ?? '',
+        category: doc.data()?['category'] ?? '',
+        subCategory: doc.data()?['sub_category'] ?? '',
+        createdByUID: doc.data()?['created_by_uid'] ?? '',
+        imageurl: doc.data()?['image_url'] ?? '');
+  }
+}
