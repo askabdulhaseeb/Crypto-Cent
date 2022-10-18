698c2ffe83914834810ae1d2fec19c429161e4cc
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-18 20:24:32 +0500

    heart png upload

--- lib/screens/home_screen/product_full_screen/product_full_screen.dart
+++ lib/screens/home_screen/product_full_screen/product_full_screen.dart
@@ -56,7 +56,7 @@ class _ProductFullScreenState extends State<ProductFullScreen> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
+                SizedBox(
                   height: 250,
                   width: MediaQuery.of(context).size.width * 0.8,
                   child: Image(
@@ -145,116 +145,119 @@ class _ProductFullScreenState extends State<ProductFullScreen> {
           height: MediaQuery.of(context).size.height * 0.55,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const <Widget>[
-                    Icon(
-                      Icons.cancel,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      child: Image(
-                        image: NetworkImage(widget.product.imageurl),
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          width: 250,
-                          child: Text(
-                            widget.product.productname.toString(),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                letterSpacing: 0.5,
-                                fontWeight: FontWeight.w900,
-                                fontSize: 18),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          '\$${widget.product.amount}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    ForText(
-                      name: 'Quantity',
-                      bold: true,
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      onPressed: quantity < 2
-                          ? null
-                          : () {
-                              setState(() {
-                                quantity--;
-                              });
-                            },
-                      icon: const Icon(
-                        Icons.remove_circle_outline,
-                        size: 15,
-                      ),
-                    ),
-                    Text(
-                      quantity.toString(),
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        print('add press');
-                        setState(() {
-                          quantity++;
-                        });
-                      },
-                      icon: const Icon(Icons.add_circle_outline, size: 15),
-                    ),
-                  ],
-                ),
-                Expanded(child: Container()),
-                (cartPro.checkExit(widget.product))
-                    ? const Icon(
-                        Icons.done,
-                        color: Colors.green,
-                        size: 29,
+            child: StatefulBuilder(builder: (BuildContext context, setState) {
+              return Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        icon: const Icon(Icons.close),
                       )
-                    : CustomElevatedButton(
-                        title: 'Add to cart',
-                        onTap: () {
-                          cartPro.addtocart(widget.product, quantity);
-                        }),
-                const SizedBox(height: 20),
-              ],
-            ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Image(
+                          image: NetworkImage(widget.product.imageurl),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 250,
+                            child: Text(
+                              widget.product.productname.toString(),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                  letterSpacing: 0.5,
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 18),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            '\$${widget.product.amount}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      ForText(
+                        name: 'Quantity',
+                        bold: true,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: quantity < 2
+                            ? null
+                            : () {
+                                setState(() {
+                                  quantity--;
+                                });
+                              },
+                        icon: const Icon(
+                          Icons.remove_circle_outline,
+                          size: 15,
+                        ),
+                      ),
+                      Text(
+                        quantity.toString(),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          print('add press');
+                          setState(() {
+                            quantity++;
+                          });
+                        },
+                        icon: const Icon(Icons.add_circle_outline, size: 15),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: Container()),
+                  (cartPro.checkExit(widget.product))
+                      ? const Icon(
+                          Icons.done,
+                          color: Colors.green,
+                          size: 29,
+                        )
+                      : CustomElevatedButton(
+                          title: 'Add to cart',
+                          onTap: () {
+                            cartPro.addtocart(widget.product, quantity);
+                            Navigator.of(context).pop();
+                          }),
+                  const SizedBox(height: 20),
+                ],
+              );
+            }),
           ),
         );
       },
