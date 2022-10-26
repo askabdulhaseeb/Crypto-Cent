4d0fa251ba6d56922b3a6f20f3df531d22415ae7
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-26 21:09:57 +0500

    encryption ok

--- lib/function/encryption_function.dart
+++ lib/function/encryption_function.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:encrypt/encrypt.dart';
@@ -5,21 +6,26 @@ import 'package:encrypt/encrypt.dart';
 import '../database/app_user/auth_method.dart';
 
 class Encryption {
- static final key = Key.fromLength(32);
-  final iv = IV.fromLength(16);
+  static String uid = AuthMethods.uid;
+  static String bs64 = base64.encode(uid.codeUnits);
+  static String x = bs64.substring(bs64.length - 24, bs64.length);
+  static final Key key = Key.fromBase64(x);
+  final IV iv = IV.fromBase64(x);
+
 
   final Encrypter encrypter = Encrypter(AES(key));
 
   String appEncrypt(String data) {
-   
+   log('bs64: $bs64');
+    log('x: $x');
     final Encrypted encrypted = encrypter.encrypt(data, iv: iv);
     final String encrypted64 = encrypted.base64;
-    log(encrypted64);
+   
     return encrypted64;
   }
 
-  String appDecrypt(String encrypted) {
-    final String decrypted64 = encrypter.decrypt64(encrypted, iv: iv);
+  String appDecrypt(String data) {
+    final String decrypted64 = encrypter.decrypt(Encrypted.fromBase64(data), iv: iv);
 
     return decrypted64;
   }
