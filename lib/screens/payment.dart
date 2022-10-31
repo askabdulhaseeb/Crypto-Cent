64198ed4df97f0a3905f091ece80955257b9dd50
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-10-31 21:05:03 +0500

    payment all ok

--- lib/screens/payment.dart
+++ lib/screens/payment.dart
@@ -37,84 +37,84 @@ class _PaymentScreenState extends State<PaymentScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const ForText(
-                name: 'Payment System',
-                bold: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const ForText(
+              name: 'Payment System',
+              bold: true,
+            ),
+            const SizedBox(height: 16),
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(26),
+                color: Theme.of(context).primaryColor,
               ),
-              const SizedBox(height: 16),
-              Container(
-                height: 200,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(26),
-                  color: Theme.of(context).primaryColor,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const ForText(
-                        name: 'Your Balance',
-                        color: Colors.white,
-                        bold: true,
-                        size: 28,
-                      ),
-                      // FutureBuilder<double>(
-                      //     future: WallletWithApi().getWalletBalance(address),
-                      //     builder: (BuildContext context,
-                      //         AsyncSnapshot<double> snapshot) {
-                      //       if (snapshot.hasData) {
-                      //         double balance = snapshot.data!;
-                      //         return Text(
-                      //           '\$${balance.toStringAsFixed(8)}',
-                      //           style: const TextStyle(
-                      //             fontSize: 20,
-                      //             fontWeight: FontWeight.w500,
-                      //             color: Colors.white,
-                      //           ),
-                      //         );
-                      //       } else {
-                      //         return snapshot.hasError
-                      //             ? const Text('ERROR')
-                      //             : const CircularProgressIndicator.adaptive();
-                      //       }
-                      //     }),
-                      const SizedBox(height: 20),
-                      Container(
-                        height: 40,
-                        width: 120,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(16)),
-                        child: const Center(child: ForText(name: 'Bitcoin')),
-                      )
-                    ],
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const ForText(
+                      name: 'Your Balance',
+                      color: Colors.white,
+                      bold: true,
+                      size: 28,
+                    ),
+                    // FutureBuilder<double>(
+                    //     future: WallletWithApi().getWalletBalance(address),
+                    //     builder: (BuildContext context,
+                    //         AsyncSnapshot<double> snapshot) {
+                    //       if (snapshot.hasData) {
+                    //         double balance = snapshot.data!;
+                    //         return Text(
+                    //           '\$${balance.toStringAsFixed(8)}',
+                    //           style: const TextStyle(
+                    //             fontSize: 20,
+                    //             fontWeight: FontWeight.w500,
+                    //             color: Colors.white,
+                    //           ),
+                    //         );
+                    //       } else {
+                    //         return snapshot.hasError
+                    //             ? const Text('ERROR')
+                    //             : const CircularProgressIndicator.adaptive();
+                    //       }
+                    //     }),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 40,
+                      width: 120,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16)),
+                      child: const Center(child: ForText(name: 'Bitcoin')),
+                    )
+                  ],
                 ),
               ),
-              const SizedBox(height: 15),
-              Row(
-                children: <Widget>[
-                  const ForText(
-                    name: 'Total',
-                    bold: true,
-                  ),
-                  const SizedBox(width: 20),
-                  ForText(
-                    name: cartPro.totalPrice().toString(),
-                    bold: true,
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              children: <Widget>[
+                const ForText(
+                  name: 'Total',
+                  bold: true,
+                ),
+                const SizedBox(width: 20),
+                ForText(
+                  name: '\$ ${cartPro.totalPrice().toString()}',
+                  bold: true,
+                ),
+              ],
+            ),
+            const Spacer(),
+            CustomElevatedButton(title: 'Pay', onTap: () {})
+          ],
         ),
       ),
     );
