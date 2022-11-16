c9d050f24ce37f6a54eb750c9756ee1f27a9f132
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-16 16:33:12 +0500

    order chart update

--- lib/screens/order/order_history.dart
+++ lib/screens/order/order_history.dart
@@ -1,12 +1,59 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class OrderHistory extends StatelessWidget {
+class OrderHistory extends StatefulWidget {
   const OrderHistory({super.key});
 
   @override
+  State<OrderHistory> createState() => _OrderHistoryState();
+}
+
+class _OrderHistoryState extends State<OrderHistory> {
+  late int showingTooltip;
+
+  @override
+  void initState() {
+    showingTooltip = -1;
+    super.initState();
+  }
+
+  BarChartGroupData generateGroupData(int x, int y) {
+    return BarChartGroupData(
+      x: x,
+      showingTooltipIndicators: showingTooltip == x ? [0] : [],
+      barRods: [
+        BarChartRodData(
+            toY: y.toDouble(),
+            color: Theme.of(context).primaryColor,
+            width: 15),
+      ],
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('OrderHistory')),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 350,
+                width: double.infinity,
+                child: BarChart(BarChartData(
+                  barGroups: [
+                    generateGroupData(1, 10),
+                    generateGroupData(2, 18),
+                    generateGroupData(3, 4),
+                    generateGroupData(4, 11),
+                  ],
+                )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
