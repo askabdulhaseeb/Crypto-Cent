5cc06c2e3dee4721b29e3ad0fae6d9b4f638dac6
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-16 17:48:12 +0500

    order histoty update

--- lib/screens/order/order_history.dart
+++ lib/screens/order/order_history.dart
@@ -1,6 +1,8 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+import '../../widgets/custom_widgets/custom_widget.dart';
+
 class OrderHistory extends StatefulWidget {
   const OrderHistory({super.key});
 
@@ -50,6 +52,56 @@ class _OrderHistoryState extends State<OrderHistory> {
                   ],
                 )),
               ),
+              SizedBox(height: 20),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: const Color(0xfff6f7f9),
+                      height: 100,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Container(
+                              height: 60,
+                              width: 60,
+                              color: Colors.white,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                ForText(
+                                  name: 'Confirmed',
+                                  bold: true,
+                                  size: 16,
+                                ),
+                                ForText(
+                                  name: 'OrderId:asdaddgfgg',
+                                  size: 14,
+                                ),
+                                ForText(
+                                  name: 'Item:7',
+                                  size: 14,
+                                ),
+                              ],
+                            ),
+                            const ForText(
+                              name: '6-9-2022',
+                              bold: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
